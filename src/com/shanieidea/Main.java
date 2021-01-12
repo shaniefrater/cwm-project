5ecb78ff31de8a31f217ef80df893f53d4ace950
shaniefrater <shaniemarie@live.co.uk>
2021-01-12 12:30:43 +0000

    committing first file

---
+++ src/com/shanieidea/Main.java
@@ -0,0 +1,10 @@
+package com.shanieidea;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        double result = Math.round(Math.random() * 200);
+        System.out.println(result);
+    }
+}
